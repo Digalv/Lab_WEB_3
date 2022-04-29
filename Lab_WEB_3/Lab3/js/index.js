f261999e4b236e60de3d00dc8202490c854a73b8
Digalv <100136401+Digalv@users.noreply.github.com>
2022-04-29 21:11:29 +0200

    Add files via upload

---
+++ Lab_WEB_3/Lab3/js/index.js
@@ -0,0 +1,9 @@
+ document.querySelector('.button').addEventListener('click', function () {
+    document.querySelector('.cont').classList.add('visibl');
+});
+
+ document.querySelector('.overlay').addEventListener('click', function () {
+    document.querySelector('.cont').classList.remove('visibl');
+});
+ 
+ 
